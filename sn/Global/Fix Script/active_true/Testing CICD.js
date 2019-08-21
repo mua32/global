2b80a16f4867bd2f97ee0441c47d4aec633d8e88
System Administrator <admin@example.com>
2019-08-21 18:57:33 +0530

    Changes by 'System Administrator' on 'CICDIntegrationB3' -- Build #1

--- sn/Global/Fix Script/active_true/Testing CICD.js
+++ sn/Global/Fix Script/active_true/Testing CICD.js
@@ -3,8 +3,8 @@
  * ClassName   : sys_script_fix
  * Created On  : 2019-07-31 11:32:51
  * Created By  : admin
- * Updated On  : 2019-08-20 14:17:57
+ * Updated On  : 2019-08-21 13:24:20
  * Updated By  : admin
  * URL         : /sys_script_fix.do?sys_id=b67b782cdb4b7300e76e771c8c961929
  */
-gs.print("Hello CICDXXxcx");
+gs.print("Hello CICDXXxcxew");
